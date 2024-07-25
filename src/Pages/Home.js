10105bde5417730cae7d4dedb8feb905191e1d05
caleb05w <99751567+caleb05w@users.noreply.github.com>
2024-07-25 09:25:00 -0700

    Added second week deliverables

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -35,6 +35,56 @@ function Home() {
       </div>
 
       </div>
+
+      <div className='min-h-lvh border-t-2 border-black min-w-lvw bg-orange-400'>
+        <div className='text-center mt-[5%]'>
+          <h1 className='text-4xl font-semibold'> Part 2 </h1>
+          <h1 className='text-2xl font-semibold'> Grids, Margins, Padding, Display, Gap, Flex</h1>
+        </div>
+
+      <div className='mt-[5] border-2 bg-black'>
+        <h1 className='text-2xl font-bold text-center mb-[5%] text-white'> Meet the Team</h1>
+
+        <div className='grid grid-cols-3 gap-[0.5%]'>
+
+          <div className='group ease-in-out duration-300 w-[100%] min-h-[30lvh] bg-black border-2 rounded-lg border-white px-[5%] py-[10%] hover:bg-white hover:border-black hover:cursor-pointer '>
+            <h1 className=' ease-in-out duration-300 group-hover:text-black text-white text-center text-4xl'>Caleb Wu</h1>
+            <h2 className='ease-in-out duration-300 group-hover:text-black text-white text-center text-2xl'>CTO</h2>
+          </div>
+
+          <div className='group ease-in-out duration-300 w-[100%] min-h-[30lvh] bg-black border-2 rounded-lg border-white px-[5%] py-[10%] hover:bg-white hover:border-black hover:cursor-pointer '>
+            <h1 className=' ease-in-out duration-300 group-hover:text-black text-white text-center text-4xl'>Brandon Sun</h1>
+            <h2 className='ease-in-out duration-300 group-hover:text-black text-white text-center text-2xl'>CFO</h2>
+          </div>
+
+          <div className='group ease-in-out duration-300 w-[100%] min-h-[30lvh] bg-black border-2 rounded-lg border-white px-[5%] py-[10%] hover:bg-white hover:border-black hover:cursor-pointer '>
+            <h1 className=' ease-in-out duration-300 group-hover:text-black text-white text-center text-4xl'>Darren Lau</h1>
+            <h2 className='ease-in-out duration-300 group-hover:text-black text-white text-center text-2xl'>CMO</h2>
+          </div>
+
+        </div>
+
+        <div className='mt-[0.5%] flex gap-[0.5%]'>
+
+        <div className='group ease-in-out duration-300 w-[70%] max-h-[5vh] bg-black border-2 rounded-lg border-white px-[5%] py-[10%] hover:bg-white hover:border-black hover:cursor-pointer '>
+            <h1 className=' ease-in-out duration-300 group-hover:text-black text-white text-center text-4xl'>Darren Lau</h1>
+            <h2 className='ease-in-out duration-300 group-hover:text-black text-white text-center text-2xl'>CMO</h2>
+          </div>
+
+          
+        <div className='group ease-in-out duration-300 w-[30%] max-h-[5vh] bg-black border-2 rounded-lg border-white px-[5%] py-[10%] hover:bg-white hover:border-black hover:cursor-pointer '>
+            <h1 className=' ease-in-out duration-300 group-hover:text-black text-white text-center text-4xl'>Darren Lau</h1>
+            <h2 className='ease-in-out duration-300 group-hover:text-black text-white text-center text-2xl'>CMO</h2>
+          </div>
+
+        </div>
+
+        <div className='my-[5%]'>
+        <h1 className='text-lg text-center text-white'> First 3 boxes are created using grid columns, the last 2 boxes are created using display flex</h1>
+        </div>
+      </div>
+
+      </div>
     </div>
   )
 }
