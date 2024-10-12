13dd6574e61979f220aa8d324577eb93ee90033d
caleb05w <99751567+caleb05w@users.noreply.github.com>
2024-10-12 14:20:03 -0700

    removed decap branches

--- src/Components/AnimatedRoutes.js
+++ src/Components/AnimatedRoutes.js
@@ -6,8 +6,8 @@ import { BrowserRouter as Router, Route, Routes, useLocation } from "react-route
 //pages
 import Home from '../Pages/Home.js'
 import About from '../Pages/About.js'
-import Blog from '../Pages/Blog.js'
-import BlogPage from '../Pages/BlogPage.js'
+// import Blog from '../Pages/Blog.js'
+// import BlogPage from '../Pages/BlogPage.js'
 import Map from '../Pages/Map.js'
 import Shop from '../Pages/Shop.js'
 
@@ -27,8 +27,8 @@ const location = useLocation();
         <Routes location={location} key={location.pathname}>
         <Route exact path='/' element={<Home />} />
         <Route exact path='/about' element={<About />} />
-        <Route exact path='/blog' element={<Blog />} />
-        <Route exact path='/blogPage' element={<BlogPage />} />
+        {/* <Route exact path='/blog' element={<Blog />} />
+        <Route exact path='/blogPage' element={<BlogPage />} /> */}
         <Route exact path='/shop' element={<Shop />} />
         <Route exact path='/map' element={<Map />} />
 
