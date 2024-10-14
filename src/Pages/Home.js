0e816965ad95ca35e4501afdf2080a7ddaf1cf90
Brandon Sun <brandonsun75@gmail.com>
2024-10-13 20:15:47 -0700

    trying to push

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -20,7 +20,7 @@ function Home() {
       <div className='mt-[4%] w-[80%] p-[2%] bg-green-800 text-white flex m-auto border-2 rounded-2xl'>
         <div className='w-[40%]'>
           <h2>New!(Img)</h2>
-          <h1 className='mt-[5%] font-bold'>Citrus Candies</h1>
+          <h1 className='mt-[5%] font-bold'>Citrus Candies asdf</h1>
           <h4 className='mt-[3%] font-bold'>7.99 Per Package</h4>
           <h4 className='mt-[3%] font-bold'>Nutritious vitamin-filled, citrus delicacies hand-made from repurposed juiced oranges. Made to enjoy in class, on a drive, during study sessions, basically anywhere!</h4>
 
